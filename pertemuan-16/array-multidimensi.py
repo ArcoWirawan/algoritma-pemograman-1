dca80e44897603ba3ba1655c6c1650ae25a8efd6
ArcoWirawan <wrnbcknd@gmail.com>
2025-11-28 20:27:16 +0700

    28 nov 2025 - Pertemuan 16

---
+++ pertemuan-16/array-multidimensi.py
@@ -0,0 +1,18 @@
+# contoh 1 : matrix 3 x 3
+matrix1 = [[1,2,3], [4,5,6], [7,8,9]]
+print(f"Contoh 1 : {matrix1[0][1]}") # mengambil angka 2 
+
+print()
+# contoh 2 : Matrix 3 x 2
+matrix2 = [[10, 20], [30,40], [50, 60]]
+print(f"contoh 2 : {matrix2}")
+
+
+# contoh 3 : Matrix Karakter
+matrix3 = [["a","b","c"], ["d", "e", "f"]]
+print(f"Contoh 3 : {matrix3}")
+
+# contoh 4 : Data mahasiswa
+
+mhs = [["Arco Wirawan", 20, "Informatika"], ["Badi", 18, "Ilmu Komunikasi"]]
+print(f"Contoh 4 = {mhs}")
