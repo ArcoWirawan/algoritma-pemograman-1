e11f63b4ff3ea6c8bf5239c845f3a4d3234ac11f
ArcoWirawan <wrnbcknd@gmail.com>
2025-12-05 20:21:17 +0700

    5 des 2025

---
+++ pertemuan-19/function.py
@@ -0,0 +1,21 @@
+#1. no parameter function
+def Hi():
+    print("Hai, sayang")
+    
+print("1. fungsi tanpa parameter")
+Hi() # cara memanggil function
+
+
+# 2. Fungsi dengan satu parameter
+def Sapa(name):
+    print(f"Halo, {name}")
+    
+print("\n2. Fungsi dengan satu parameter")
+Sapa("aarwannzxp")
+
+# 3. Fungsi dengan dua parameter
+def Addtion(a,b):
+    print(f"{a} + {b} = {a + b}")
+    
+print("\n3. Fungsi dengan dua parameter")
+Addtion(7,11)
